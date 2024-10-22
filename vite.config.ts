9d1a05899c16722d046308fd7ff83ee7edf78ac3
forestvov1 <kukva2019@gmail.com>
2024-10-22 15:21:59 +1000

    fix sass

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import Unfonts from 'unplugin-fonts/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            },
+        },
+    },
     plugins: [
         react(),
         svgr(),
